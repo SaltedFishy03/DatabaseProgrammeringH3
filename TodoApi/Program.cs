d51c9553b1faf2f6a20b6a36c7e17a4b50a1ccf1
SaltedFishy03 <Seekerbrutus@gmail.com>
2024-11-08 12:53:35 +0100

    fixed double program started

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -18,18 +18,6 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-
-    // Automatically open Swagger in the default browser
-    var url = "http://localhost:5213/swagger"; // Update port if needed
-    Task.Run(() => {
-        // Delay for a moment to ensure the app is fully started
-        Thread.Sleep(1000); 
-        Process.Start(new ProcessStartInfo
-        {
-            FileName = url,
-            UseShellExecute = true // This opens the URL in the default browser
-        });
-    });
 }
 else
 {
